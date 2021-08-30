3086d58f9800e60464893306aaff15ec07cd0703
chinaranasibova <chinarahn@code.edu.az>
2021-08-30 06:48:45 +0400

    created Index.html & payroll.html

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+  $(".dropdownn").click(function (e) {
+    e.preventDefault();
+  
+    if ($(this).find(".ul-listt").css('display') == 'none') {
+      $(this).find('svg').css('transform', 'rotate(-180deg)');
+    }
+    else {
+      $(this).find('svg').css('transform', 'rotate(0deg)');
+    }
+
+
+
+    $(".ul-listt").slideToggle();
+
+  });
+
+
+
+
+
+  $(".dropdownright").click(function (e) {
+    e.preventDefault();
+
+    if ($(this).find(".dropdown-right").css('display') == 'none') {
+      $(this).find('svg').css('transform', 'rotate(-180deg)');
+    }
+    else {
+      $(this).find('svg').css('transform', 'rotate(0deg)');
+    }
+
+    $(".dropdown-right").slideToggle();
+
+  })
+
+
+
+  $('.image-icon').click(function (e) {
+    
+    if ($(this).parent().next(".accordion-contentt").css('display') == 'none') {
+      $(this).find('img').css('transform', 'rotate(90deg)');
+    }
+    else {
+      // $(this).parent().next().css('transform', 'rotate(0deg)');
+      $(this).find('img').css('transform', 'rotate(0)');
+
+    }
+
+
+
+    $(this).parent().next().slideToggle();
+  })
+
+});
+
